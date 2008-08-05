63bef0f8cdea980eee262d1a88c7db7283d46e97
activefx <activefx@yahoo.com>
2008-08-05 01:17:29 -0400

    removed include AuthenticatedSystem from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
+
   
 
   # render new.rhtml
