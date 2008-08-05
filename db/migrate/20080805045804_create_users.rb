ec9d5b248427b1f6f039f742d4c804147bfa2b0b
activefx <activefx@yahoo.com>
2008-08-05 05:27:24 -0400

    added password reset code, enabled, and identity url to the user migration

--- db/migrate/20080805045804_create_users.rb
+++ db/migrate/20080805045804_create_users.rb
@@ -12,6 +12,9 @@ class CreateUsers < ActiveRecord::Migration
       t.column :remember_token_expires_at, :datetime
       t.column :activation_code,           :string, :limit => 40
       t.column :activated_at,              :datetime
+ 		  t.column :password_reset_code,       :string, :limit => 40
+      t.column :enabled,                   :boolean, :default => true   
+			t.column :identity_url,							 :string 
 
     end
     add_index :users, :login, :unique => true
