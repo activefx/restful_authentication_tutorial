68d396c5e9adea1082af7720edc38500754e9930
activefx <activefx@yahoo.com>
2008-08-05 03:15:51 -0400

    added a passwords controller to enable resetting of forgotten passwords

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,72 @@
+class PasswordsController < ApplicationController
+  
+  # Enter email address to recover password 
+  def new
+  end
+
+  # Forgot password action
+  def create    
+    if @user = User.find_for_forget(params[:email])
+      @user.forgot_password
+      @user.save      
+      flash[:notice] = "A password reset link has been sent to your email address."
+      redirect_to root_path
+    else
+			logger.warn "Invalid email entered '#{params[:email]}' from #{request.remote_ip} at #{Time.now.utc}"
+      flash[:notice] = "Could not find a user with that email address."
+      render :action => 'new'
+    end  
+  end
+  
+  # Action triggered by clicking on the /reset_password/:id link recieved via email
+  # Makes sure the id code is included
+  # Checks that the id code matches a user in the database
+  # Then if everything checks out, shows the password reset fields
+  def edit
+    if params[:id].nil?
+      render :action => 'new'
+      return
+    end
+    @user = User.find_by_password_reset_code(params[:id]) if params[:id]
+    raise if @user.nil?
+    rescue
+    logger.warn "Invalid password reset code from #{request.remote_ip} at #{Time.now.utc}"
+    flash[:notice] = "Sorry - That is an invalid password reset code. Please check your code and try again. (Perhaps your email client inserted a carriage return?)"
+    redirect_to new_user_path
+  end
+    
+  # Reset password action /reset_password/:id
+  # Checks once again that an id is included and makes sure that the password field isn't blank
+  def update
+    if params[:id].nil?
+      render :action => 'new'
+      return
+    end
+    if (params[:password].blank? || params[:password_confirmation].blank?)
+      flash[:notice] = "Password fields cannot be blank."
+      render :action => 'edit', :id => params[:id]
+      return
+    end
+    @user = User.find_by_password_reset_code(params[:id]) if params[:id]
+    raise if @user.nil?
+    return if @user unless params[:password]
+      if (params[:password] == params[:password_confirmation])
+        @user.password_confirmation = params[:password_confirmation]
+        @user.password = params[:password]
+        @user.reset_password        
+        flash[:notice] = @user.save ? "Password reset." : "There was a problem resetting your password."
+      else
+        flash[:notice] = "Password mismatch."
+        render :action => 'edit', :id => params[:id]
+        return
+      end  
+      redirect_to login_path
+    rescue
+    logger.warn "Invalid password reset code from #{request.remote_ip} at #{Time.now.utc}"
+    flash[:notice] = "Sorry - That is an invalid password reset code. Please check your code and try again. (Perhaps your email client inserted a carriage return?)"
+    redirect_to new_user_path
+  end
+    
+end
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
       flash[:error] = "The activation code was missing.  Please follow the URL from your email."
       redirect_back_or_default('/')
     else 
+			logger.warn "Invalid activation code from #{request.remote_ip} at #{Time.now.utc}"
       flash[:error]  = "We couldn't find a user with that activation code -- check your email? Or maybe you've already activated -- try signing in."
       redirect_back_or_default('/')
     end
