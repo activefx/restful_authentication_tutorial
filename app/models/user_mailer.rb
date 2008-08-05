d99e73e47558a6a64aef7cd40ca70eda2e4a93bd
activefx <activefx@yahoo.com>
2008-08-05 03:40:23 -0400

    added forgot and reset password methods to user mailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,9 +2,7 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
-  
+    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}" 
   end
   
   def activation(user)
@@ -12,7 +10,18 @@ class UserMailer < ActionMailer::Base
     @subject    += 'Your account has been activated!'
     @body[:url]  = "http://YOURSITE/"
   end
-  
+
+  def forgot_password(user)
+    setup_email(user)
+    @subject    += 'You have requested to change your password'
+    @body[:url]  = "http://localhost:3000/reset_password/#{user.password_reset_code}"
+  end
+
+  def reset_password(user)
+    setup_email(user)
+    @subject    += 'Your password has been reset.'
+  end
+
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
