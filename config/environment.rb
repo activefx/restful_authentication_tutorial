da50211f299686466e76762e5ab25c3fbe612ceb
activefx <activefx@yahoo.com>
2008-08-05 05:10:30 -0400

    added ruby-openid gem dependency and activated user_observer to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+	config.gem "ruby-openid", :version => '2.1.2'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
@@ -64,4 +65,5 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+	config.active_record.observers = :user_observer
 end
